544c2de59d9cd2db988dab1fc6520f5375c52cf2
Mike Bond <mikebond84@gmail.com>
2019-03-05 11:26:19 -0500

    deploy to firebase fixed

---
+++ Michael_Bond/app/js/firebase.js
@@ -0,0 +1,8 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyBljBfCxlIaOmNPuDE99nxBfFrMWTa36GI",
+  databaseURL: "https://michael-bond.firebaseio.com",
+  storageBucket: "michael-bond.appspot.com",
+  authDomain: "michael-bond.firebaseapp.com",
+  messagingSenderId: "1069723899420",
+  projectId: "michael-bond"
+});
