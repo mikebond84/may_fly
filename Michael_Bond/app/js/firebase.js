d2d6f7bf0e38ff74a9c7f8d69e1002cd6a770a09
Mike Bond <mikebond84@gmail.com>
2019-03-19 10:55:59 -0400

    color palette final

--- Michael_Bond/app/js/firebase.js
+++
@@ -1,8 +0,0 @@
-firebase.initializeApp({
-  apiKey: "AIzaSyBljBfCxlIaOmNPuDE99nxBfFrMWTa36GI",
-  databaseURL: "https://michael-bond.firebaseio.com",
-  storageBucket: "michael-bond.appspot.com",
-  authDomain: "michael-bond.firebaseapp.com",
-  messagingSenderId: "1069723899420",
-  projectId: "michael-bond"
-});
